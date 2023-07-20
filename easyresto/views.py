5ec7b3c9cc0d7a9146757cec32f120f7ff096e14
Himanshu <himanshunpatil04@gmail.com>
2023-07-20 21:37:19 +0530

    postgres config

--- easyresto/views.py
+++ easyresto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(requests):
-    return HttpResponse('<h1>Hello World</h1>')
+def home(request):
+    return render(request, 'home.html')
