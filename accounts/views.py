77d75234ee91df1e59a331e29cddbe4d4f3c5ae7
Himanshu <himanshunpatil04@gmail.com>
2023-07-23 01:03:28 +0530

    vendor customer dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 # Restrict the vendor from accessing customer page
 def check_role_vendor(user):
@@ -163,7 +164,11 @@ def customerDashboard(request):
 @user_passes_test(check_role_vendor)
 @login_required(login_url='login')
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user = request.user)
+    context={
+        'vendor':vendor,
+    }
+    return render(request, 'accounts/vendorDashboard.html', context)
 
 
 def forgot_password(request):
