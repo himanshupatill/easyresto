cc3166a8bb02de5ff3509c073498575545cf7dc1
Himanshu <himanshunpatil04@gmail.com>
2023-08-14 01:20:59 +0530

    orders and order detail features

--- easyresto/settings.py
+++ easyresto/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'orders.request_object.RequestObjectMiddleware',
 ]
 
 ROOT_URLCONF = 'easyresto.urls'
